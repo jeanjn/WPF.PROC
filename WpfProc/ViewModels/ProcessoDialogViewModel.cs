8828fff430c739fb04562ea23749032d58236f2c
Jean Carlos de Almeida <jeanjnx@gmail.com>
2016-08-31 16:06:09 -0300

    DataBinding Processo

--- WpfProc/ViewModels/ProcessoDialogViewModel.cs
+++ WpfProc/ViewModels/ProcessoDialogViewModel.cs
@@ -1,4 +1,6 @@
-﻿using PROC.Model;
+﻿using MaterialDesignThemes.Wpf;
+using PROC.DataAccess;
+using PROC.Model;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -6,12 +8,139 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfProc.Helpers;
 
 namespace WpfProc.ViewModels
 {
     public class ProcessoDialogViewModel : INotifyPropertyChanged
     {
-        public Andamento SelectedAndamento { get; set; }
+        private Context _context;
+
+        private Processo _processo;
+
+        #region Processo
+        public Processo Processo { get; set; }
+        public DateTime Data
+        {
+            get
+            {
+                return Processo.Data;
+            }
+            set
+            {
+                Processo.Data = value;
+                NotifyPropertyChanged(nameof(Data));
+            }
+        }
+        public string Numero
+        {
+            get
+            {
+                return Processo.Numero;
+            }
+            set
+            {
+                Processo.Numero = value;
+                NotifyPropertyChanged(nameof(Data));
+            }
+        }
+        public string Descricao
+        {
+            get
+            {
+                return Processo.Descricao;
+            }
+            set
+            {
+                Processo.Descricao = value;
+                NotifyPropertyChanged(nameof(Descricao));
+            }
+        }
+        public string Observacao
+        {
+            get
+            {
+                return Processo.Observacao;
+            }
+            set
+            {
+                Processo.Observacao = value;
+                NotifyPropertyChanged(nameof(Observacao));
+            }
+        }
+        public DateTime? Lembrete
+        {
+            get
+            {
+                return Processo.Lembrete ?? DateTime.Now;
+            }
+            set
+            {
+                Processo.Lembrete = value;
+                SetLembrete();
+                NotifyPropertyChanged(nameof(Lembrete));
+            }
+        }
+        public string _tempoLembrete { get; set; }
+        public string TempoLembrete
+        {
+            get
+            {
+                return _tempoLembrete;
+            }
+            set
+            {
+                _tempoLembrete = value;
+                SetLembrete();
+                NotifyPropertyChanged(nameof(TempoLembrete));
+            }
+        }
+        public Andamento Andamento
+        {
+            get
+            {
+                return Processo.Andamento;
+            }
+            set
+            {
+                Processo.Andamento = value;
+                NotifyPropertyChanged(nameof(Andamento));
+            }
+        }
+
+        private void SetLembrete()
+        {
+            if (Lembrete == null)
+            {
+                return;
+            }
+
+            int hora = 0, minuto = 0;
+            try
+            {
+                
+
+                var result = string.IsNullOrWhiteSpace(TempoLembrete) ? new string[] { } : TempoLembrete.Split(':');
+                if (result.Length > 1)
+                {
+                    hora = Convert.ToInt32(result[0]);
+                    minuto = Convert.ToInt32(result[1]);
+                }
+
+                var lembrete = Lembrete.Value;
+                Processo.Lembrete = new DateTime(lembrete.Year, lembrete.Month, lembrete.Day, hora, minuto, 0, 0);
+            }
+            catch (Exception)
+            {
+                Lembrete = null;
+                TempoLembrete = null;
+            }
+
+        }
+        #endregion
+
+
 
         private ObservableCollection<Andamento> _andamentos;
         public ObservableCollection<Andamento> Andamentos
@@ -25,6 +154,26 @@ namespace WpfProc.ViewModels
                     NotifyPropertyChanged(nameof(_andamentos));
                 }
             }
+        }
+
+        public ICommand Salvar => new Command<ProcessoDialogViewModel>(a =>
+        {
+            SalvarProcesso();
+        });
+
+
+        public ProcessoDialogViewModel()
+        {
+            _context = new Context();
+
+            Andamentos = new ObservableCollection<Andamento>(_context.Andamentos);
+
+            Processo = new Processo();
+        }
+
+        private void SalvarProcesso()
+        {
+            DialogHost.CloseDialogCommand.Execute(null, null);
         }
 
 
--- WpfProc/Views/NovoProcessoDialog.xaml.cs
+++ WpfProc/Views/NovoProcessoDialog.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfProc.ViewModels;
 
 namespace WpfProc.Views
 {
@@ -23,6 +24,7 @@ namespace WpfProc.Views
         public NovoProcessoDialog()
         {
             InitializeComponent();
+            DataContext = new ProcessoDialogViewModel();
         }
     }
 }
